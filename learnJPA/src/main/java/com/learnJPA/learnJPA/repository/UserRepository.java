2288d647fe45a127cac2a6194fb698254f86a38e
= <=>
2023-12-03 17:48:17 +0530

    Custom finder method

--- learnJPA/src/main/java/com/learnJPA/learnJPA/repository/UserRepository.java
+++ learnJPA/src/main/java/com/learnJPA/learnJPA/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.learnJPA.learnJPA.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+    // https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html#jpa.query-methods.query-creation
     public User findByName(String name);
 }
